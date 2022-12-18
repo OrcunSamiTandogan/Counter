2cc553fb6102a7181289028f17b582139bd1d705
Orcun Sami Tandogan <73159487+OrcunSamiTandogan@users.noreply.github.com>
2022-12-18 16:19:26 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-let saveEl = document.getElementById("save-el")
+let saveEl  = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let clearEl = document.getElementById("clear-el")
+
 let count = 0
 
 function increment() {
@@ -7,9 +9,13 @@ function increment() {
     countEl.textContent = count
 }
 
+function clearX() { 
+    clearEl.textContent = "Are you happy?" 
+} 
+
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
-}
+} 
